da230b8b7e50519c477582f83d626e43c418fdcf
alone <bogdan_yalta@mail.ru>
2023-06-09 13:49:03 +0300

    Добавил page Main

---
+++ src/pages/Main/ui/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import { FC, memo } from "react";
+
+import cl from './MainPage.module.scss'
+
+const MainPage: FC = () => {
+    return (
+        <div className={cl.main_page}>
+            
+        </div>
+    );
+};
+
+export default memo(MainPage)
