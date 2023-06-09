e359a6b25afda3a9734f9bc6e4d6df4d9ff93af8
alone <bogdan_yalta@mail.ru>
2023-06-09 14:58:22 +0300

    Добавил UI Text

---
+++ src/shared/ui/Text/Text.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import classNames from "classnames";
+
+import cl from "./Text.module.scss";
+import { TextProps } from "./Text.types";
+
+export const Text: FC<TextProps> = (props) => {
+    const {
+        className,
+        children,
+        Tag = "span",
+        theme = "primary",
+        size = "14_400",
+        ...otherProps
+    } = props;
+
+    return (
+        <Tag
+            className={classNames(cl.text, cl[theme], cl[`size_${size}`], className)}
+            {...otherProps}
+        >
+            {children}
+        </Tag>
+    );
+};
+
---
+++ src/shared/ui/Text/Text.types.ts
@@ -0,0 +1,17 @@
+import { HTMLAttributes, ReactNode } from "react";
+
+export type TextTheme = "green" | "red" | "primary" | "secondary" | "purple"
+
+export type TextTag = "span" | "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+export type TextSize = "20_600" | "14_400" | "14_600";
+
+export type TextAttributes = HTMLAttributes<HTMLSpanElement | HTMLParagraphElement>;
+
+export interface TextProps extends TextAttributes {
+    children: ReactNode;
+    Tag?: TextTag;
+    theme?: TextTheme;
+    size?: TextSize;
+}
+
