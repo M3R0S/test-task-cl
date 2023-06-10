cfafa5aa006915745cd60b452744a8c7fe246d00
alone <bogdan_yalta@mail.ru>
2023-06-10 15:10:35 +0300

    Обновил Input под дизайн систему

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,16 +1,51 @@
-import { FC, memo } from "react";
+import { FC, forwardRef, memo } from "react";
 import classNames from "classnames";
 
 import cl from "./Input.module.scss";
 import { InputProps } from "./Input.types";
+import { VStack } from "../Stack";
+import { Text } from "../Text";
 
-export const Input: FC<InputProps> = memo((props) => {
-    const { className, theme = "primary", size = "300", ...otherProps } = props;
+export const Input: FC<InputProps> = memo(
+    forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+        const {
+            className,
+            theme = "primary",
+            size = "300",
+            label,
+            validateMessage,
+            isValidate = true,
+            isDirty = false,
+            ...otherProps
+        } = props;
 
-    return (
-        <input
-            className={classNames(cl.input, className, cl[theme], cl[`size_${size}`])}
-            {...otherProps}
-        />
-    );
-});
+        const isValid = !validateMessage; // Если сообщение есть - значит инпут не валидный
+
+        return (
+            <VStack gap="8">
+                {label && <label className={cl.label}>{label}</label>}
+                <input
+                    ref={ref}
+                    className={classNames(
+                        cl.input,
+                        className,
+                        cl[theme],
+                        { [cl.valid]: isValid && isDirty, [cl.not_valid]: !isValid },
+                        cl[`size_${size}`]
+                    )}
+                    {...otherProps}
+                />
+                {isValidate && (
+                    <Text
+                        Tag="p"
+                        className={cl.text}
+                        size="12_400"
+                        theme="red"
+                    >
+                        {validateMessage}
+                    </Text>
+                )}
+            </VStack>
+        );
+    })
+);
--- src/shared/ui/Input/Input.types.ts
+++ src/shared/ui/Input/Input.types.ts
@@ -1,11 +1,14 @@
 import { InputHTMLAttributes } from "react";
 
-export type InputTheme = "primary" | "secondary"
-export type InputSize = "300" | "400"
+export type InputTheme = "primary" | "secondary";
+export type InputSize = "300" | "400";
 
 export interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "size"> {
     className?: string;
-    theme?: InputTheme
-    size?: InputSize
+    theme?: InputTheme;
+    size?: InputSize;
+    label?: string;
+    validateMessage?: string; // Ошибка валидации
+    isValidate?: boolean // Нужна ли валидация
+    isDirty?: boolean // Были ли изменения
 }
-
