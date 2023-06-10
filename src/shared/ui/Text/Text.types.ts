aeee0c01c3791f75536b5b4aec93c42eb50d4d38
alone <bogdan_yalta@mail.ru>
2023-06-10 15:11:20 +0300

    Добавил новый размер

--- src/shared/ui/Text/Text.types.ts
+++ src/shared/ui/Text/Text.types.ts
@@ -4,7 +4,7 @@ export type TextTheme = "green" | "red" | "primary" | "secondary" | "purple"
 
 export type TextTag = "span" | "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
-export type TextSize = "20_600" | "14_400" | "14_600";
+export type TextSize = "20_600" | "14_400" | "14_600" | "12_400";
 
 export type TextAttributes = HTMLAttributes<HTMLSpanElement | HTMLParagraphElement>;
 
