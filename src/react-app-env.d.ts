83edf1e750d30492068605d40f55401761c5b5f0
alone <bogdan_yalta@mail.ru>
2023-06-11 14:23:53 +0300

    Описал тип ValueOf

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="react-scripts" />
 
+/**
+ * Тип, используюмый, как альтернатива enum.
+ * 
+ * @param T - Необходимо передать typeof Object as const.
+ * 
+ * @returns Unit type и объект с значениями.
+ */
 type ValueOf<T> = T[keyof T];
