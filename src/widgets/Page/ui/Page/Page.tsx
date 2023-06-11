24125dc2ed1ea47917d181f0bc26b6a3489650ee
alone <bogdan_yalta@mail.ru>
2023-06-11 21:04:40 +0300

    Обновил Page

--- src/widgets/Page/ui/Page/Page.tsx
+++ src/widgets/Page/ui/Page/Page.tsx
@@ -4,17 +4,8 @@ import classNames from "classnames";
 import cl from "./Page.module.scss";
 import { PageProps } from "./Page.types";
 
-import { VStack } from "shared/ui/Stack";
-
 export const Page: FC<PageProps> = (props) => {
     const { className, children } = props;
 
-    return (
-        <VStack
-            Tag="main"
-            className={classNames(cl.page, className)}
-        >
-            {children}
-        </VStack>
-    );
+    return <main className={classNames(cl.page, className)}>{children}</main>;
 };
