d1078745df41ecfdb3b417ac4474e34459e566a6
alone <bogdan_yalta@mail.ru>
2023-06-11 21:27:55 +0300

    Обновил Main Page

--- src/pages/Main/ui/MainPage/MainPage.tsx
+++ src/pages/Main/ui/MainPage/MainPage.tsx
@@ -5,16 +5,19 @@ import cl from "./MainPage.module.scss";
 import { Page } from "widgets/Page";
 import { EditForm } from "features/EditForm";
 import { MiniProfile } from "entities/MiniProfile";
+import { VStack } from "shared/ui/Stack";
 
 const MainPage: FC = () => {
     return (
         <Page className={cl.main_page}>
-            <MiniProfile
-                name="Иван"
-                surname="Иванов"
-            />
-            <div className={cl.row} />
-            <EditForm form="main" />
+            <VStack gap="24">
+                <MiniProfile
+                    name="Иван"
+                    surname="Иванов"
+                />
+                <div className={cl.row} />
+                <EditForm form="main" />
+            </VStack>
         </Page>
     );
 };
