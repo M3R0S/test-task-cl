4d4dd758cedbc89372ddcc18981ff920fe1a853e
alone <bogdan_yalta@mail.ru>
2023-06-14 15:00:05 +0300

    Обновил визаул у Input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from "react";
+import { forwardRef, memo } from "react";
 import classNames from "classnames";
 
 import cl from "./Input.module.scss";
@@ -6,50 +6,54 @@ import { InputProps } from "./Input.types";
 import { VStack } from "../Stack";
 import { Text } from "../Text";
 
-export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-    const {
-        className,
-        theme = "primary",
-        label,
-        validateMessage,
-        isValidate = true,
-        isDirty = false,
-        name,
-        ...otherProps
-    } = props;
+export const Input = memo(
+    forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+        const {
+            className,
+            theme = "primary",
+            label,
+            validateMessage,
+            isValidate = true,
+            isDirty = false,
+            name,
+            ...otherProps
+        } = props;
 
-    const isValid = !validateMessage; // Если сообщение есть - значит инпут не валидный
+        const isValid = !validateMessage; // Если сообщение есть - значит инпут не валидный
 
-    return (
-        <VStack gap="8">
-            {label && (
-                <label
-                    htmlFor={name}
-                    className={cl.label}
-                >
-                    {label}
-                </label>
-            )}
-            <input
-                name={name}
-                id={name}
-                ref={ref}
-                className={classNames(cl.input, className, cl[theme], {
-                    [cl.valid]: isValid && isDirty,
-                    [cl.not_valid]: !isValid,
-                })}
-                {...otherProps}
-            />
-            {isValidate && (
-                <Text
-                    Tag="p"
-                    className={cl.text}
-                    size="12_400"
-                    theme="red"
-                >
-                    {validateMessage}
-                </Text>
-            )}
-        </VStack>
-    );
-});
+        return (
+            <VStack gap="8">
+                {label && (
+                    <Text
+                        Tag="label"
+                        htmlFor={name}
+                        theme="primary"
+                        size="14_400"
+                    >
+                        {label}
+                    </Text>
+                )}
+                <input
+                    name={name}
+                    id={name}
+                    ref={ref}
+                    className={classNames(cl.input, className, cl[theme], {
+                        [cl.valid]: isValid && isDirty,
+                        [cl.not_valid]: !isValid,
+                    })}
+                    {...otherProps}
+                />
+                {isValidate && (
+                    <Text
+                        Tag="p"
+                        className={cl.text}
+                        size="12_400"
+                        theme="red"
+                    >
+                        {validateMessage}
+                    </Text>
+                )}
+            </VStack>
+        );
+    })
+);
